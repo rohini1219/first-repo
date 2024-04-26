fdd956af813f670c9aa56b6dee555b47ee9c4f3d
rohini1219 <rohinipeddapati1219@gmail.com>
2024-04-26 11:29:26 +0530

    first commit

---
+++ Pattern4.java
@@ -0,0 +1,13 @@
+public class Pattern4{
+public static void main(String[] args){
+  pattern(10);
+  }
+  static void pattern(int n){
+for( int row=1;row<=n;row++){
+ for(int col=1;col<=row;col++){
+   System.out.print(col);
+ }
+    System.out.println();
+   }
+  }
+ }
